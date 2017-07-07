4478238cdff927f0f5c2b787b969084d671a0c15
Romserg <serg.romankov@gmail.com>
2017-07-07 13:29:43 +0300

    complete js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 'use strict';
 
-console.log($)
+        
+var inputText = $('form.message-form .message-field');
+var ouputText = $('main > .message-formatted');
+var formattedText = $('main > .message-form');
+
+function formatText(){
+         ouputText.text(
+         inputText.val()
+                  .toLowerCase()
+                  .trim()
+                  .replace(/\s+/g, ' '));
+}
+
+// function cleanOutput(){
+//     ouputText.text('');
+// }
+
+inputText.on('keyup', formatText);
+formattedText.on('reset', cleanOutput);
